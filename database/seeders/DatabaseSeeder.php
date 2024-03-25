64705eed6391577c2994e002131cb0fd2b982ed0
fransiskus23 <151119270+fransiskus23@users.noreply.github.com>
2024-03-25 15:01:31 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
          \App\Models\User::factory(10)->create();
 
          \App\Models\User::factory()->create([
-            'name' => 'Andriaman D T',
+            'name' => 'Frans',
             'email' => 'test@example.com',
             'password' => bcrypt('test@example.com'),
         ]);
